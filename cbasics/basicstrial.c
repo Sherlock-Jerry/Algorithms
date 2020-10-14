dd46f95b0b9e4e342b098535af53139cee24c910
LijaAlex12 <lijaalexhere@gmail.com>
2020-10-14 19:54:17 +0530

    basicsctrial added

---
+++ cbasics/basicstrial.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+  int a,b,sum,i,p,j;
+  // printf("enter 2 nums:");
+  // scanf("%d%d",&a,&b);
+  //
+  // printf("%d\n",a+b);
+i=5;
+// j=i++;=>j=5,i=6
+j=++i;
+// =>j=6,i=6
+printf("%d\t%d\n",i,j);
+// p=5;
+// ++p; or p++ makes no difference
+// printf("%d\n",p );
+
+
+}

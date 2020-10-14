11bcf05aee933a5e21a1119330df6f8ae09a1afe
LijaAlex12 <lijaalexhere@gmail.com>
2020-10-14 20:00:54 +0530

    basicsctrial added

--- cbasics/basicstrial.c
+++ cbasics/basicstrial.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 void main(){
   int a,b,sum,i,p,j;
-  // printf("enter 2 nums:");
-  // scanf("%d%d",&a,&b);
-  //
+  printf("enter 2 nums:");
+  scanf("%d%d",&a,&b);
+  (a>b)?printf("%d is greater",a):printf("%d is greater",b);
+
   // printf("%d\n",a+b);
-i=5;
-// j=i++;=>j=5,i=6
-j=++i;
-// =>j=6,i=6
-printf("%d\t%d\n",i,j);
+
+
+
+
+// i=5;
+// // j=i++;=>j=5,i=6
+// j=++i;
+// // =>j=6,i=6
+// printf("%d\t%d\n",i,j);
 // p=5;
 // ++p; or p++ makes no difference
 // printf("%d\n",p );
